6b23b96b3ea615a763fc1837573107af8a70861c
Kalucza Gábor <kaluczadzsi@gmail.com>
2024-08-01 15:04:15 +0200

    Connect app to database

--- foodies-app/app/meals/page.js
+++ foodies-app/app/meals/page.js
@@ -1,5 +1,29 @@
 import React from "react";
+import classes from "./page.module.css";
+import Link from "next/link";
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
 
-export default function Meals() {
-  return <div>Meals</div>;
+export default async function Meals() {
+  const meals = await getMeals();
+
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={meals} />
+      </main>
+    </>
+  );
 }
