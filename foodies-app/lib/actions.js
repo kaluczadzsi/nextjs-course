3c19cb1166227dacddf70e1d045084a1d51cd9f4
Kalucza Gábor <kaluczadzsi@gmail.com>
2024-08-02 10:29:02 +0200

    Add basic form validation

--- foodies-app/lib/actions.js
+++ foodies-app/lib/actions.js
@@ -1,7 +1,11 @@
 "use server";
 
-import { redirect } from "next/dist/server/api-utils";
+import { redirect } from "next/navigation";
 import { saveMeal } from "./meals";
+
+function isInvalidText(text) {
+  return !text || text.trim() === "";
+}
 
 export async function shareMeal(formData) {
   const meal = {
@@ -12,6 +16,21 @@ export async function shareMeal(formData) {
     creator: formData.get("name"),
     creator_email: formData.get("email"),
   };
+
+  if (
+    isInvalidText(meal.title) ||
+    isInvalidText(meal.summary) ||
+    isInvalidText(meal.creator) ||
+    isInvalidText(meal.creator_email) ||
+    isInvalidText(meal.instructions) ||
+    !meal.creator_email.includes("@") ||
+    !meal.image ||
+    meal.image.size === 0
+  ) {
+    return {
+      message: "Invalid input.",
+    };
+  }
 
   await saveMeal(meal);
   redirect("/meals");
