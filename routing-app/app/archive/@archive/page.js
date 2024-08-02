bce05d0a8c1241aedf3a9bd60537f8a93b67f8ca
Kalucza Gábor <kaluczadzsi@gmail.com>
2024-08-02 15:20:36 +0200

    Add catch all fallback routes

--- routing-app/app/archive/@archive/page.js
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-  return <h1>Archive Page</h1>;
-}
--- routing-app/app/news/page.js
+++ routing-app/app/news/page.js
@@ -1,21 +1,13 @@
 "use client";
 
+import NewsList from "@/components/news-list";
 import { DUMMY_NEWS } from "@/dummy-news";
-import Link from "next/link";
 
 export default function Page() {
   return (
     <div>
       <h1>News Page</h1>
-      <ul className="news-list">
-        {DUMMY_NEWS.map((newsItem) => (
-          <li key={newsItem.id}>
-            <Link href={`/news/${newsItem.slug}`}>
-              <img src={`/images/news/${newsItem.image}`} fill />
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <NewsList news={DUMMY_NEWS} />
     </div>
   );
 }
