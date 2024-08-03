537000c23bf0258fe0a6ca5aeb977a42bc3aa10f
Kalucza Gábor <kaluczadzsi@gmail.com>
2024-08-03 10:02:28 +0200

    Fetch directly from the source

--- routing-app/app/(content)/news/page.js
+++ routing-app/app/(content)/news/page.js
@@ -1,12 +1,8 @@
 import NewsList from "@/components/news-list";
+import { getAllNews } from "@/lib/news";
 
 export default async function Page() {
-  const response = await fetch("http://localhost:8080/news");
-  const news = await response.json();
-
-  if (!response.ok) {
-    throw new Error("Failed to fetch news.");
-  }
+  const news = getAllNews();
 
   return (
     <div>
