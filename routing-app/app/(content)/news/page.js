040302197b1fe4f67f9f8e22d30e4199a9dce258
Kalucza Gábor <kaluczadzsi@gmail.com>
2024-08-03 09:38:28 +0200

    Add client site data fetching

--- routing-app/app/(content)/news/page.js
+++ routing-app/app/(content)/news/page.js
@@ -1,13 +1,44 @@
 "use client";
 
 import NewsList from "@/components/news-list";
-import { DUMMY_NEWS } from "@/dummy-news";
+import { useEffect, useState } from "react";
 
 export default function Page() {
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    async function fetchNews() {
+      try {
+        const response = await fetch("http://localhost:8080/news");
+        if (!response.ok) {
+          throw new Error("Failed to fetch news.");
+        }
+        const news = await response.json();
+        setNews(news);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchNews();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
   return (
     <div>
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      <NewsList news={news} />
     </div>
   );
 }
